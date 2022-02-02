c9c70dd467032376e73af3f5bdda94c00b7c300e
baguchan <shurf012@gmail.com>
2022-02-02 13:05:31 +0900

    add looking rule

--- src/main/java/baguchan/onlylooking/LookUtils.java
+++ src/main/java/baguchan/onlylooking/LookUtils.java
@@ -1,8 +1,9 @@
 package baguchan.onlylooking;
 
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.ClipContext;
+import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 
 public class LookUtils {
@@ -12,7 +13,25 @@ public class LookUtils {
 		double d0 = vec31.length();
 		vec31 = vec31.normalize();
 		double d1 = vec3.dot(vec31);
-		return d1 > 1.0D - 0.05D / d0 ? entity.hasLineOfSight(target) : false;
+		/*
+		 * range 1.0 is only Look at the hostile body beyond the mob's line of sight.
+		 * If range is 8.0, move the eyes of the mob to see the mob in the visible range.
+		 *
+		 */
+		double range = 8.0D;
 
+		return d1 > 1.0D - range / d0 ? LookUtils.hasLineOfSight(entity, target) : false;
+
+	}
+
+	public static boolean hasLineOfSight(LivingEntity entity, Entity target) {
+		if (target.level != entity.level) {
+			return false;
+		} else {
+			Vec3 vec3 = new Vec3(entity.getX(), entity.getEyeY(), entity.getZ());
+			Vec3 vec31 = new Vec3(target.getX(), target.getEyeY(), target.getZ());
+
+			return entity.level.clip(new ClipContext(vec3, vec31, ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, entity)).getType() == HitResult.Type.MISS;
+		}
 	}
 }
